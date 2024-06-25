3ffb92e587527c294e872bc255cc048c854a3621
loftyyyy <lofty08.business@gmail.com>
2024-06-26 01:11:09 +0800

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 const cells = document.querySelectorAll('td');
-const buttons = ["cell-0", "cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6", "cell-7", "cell-8"];
+// const buttons = ["cell-0", "cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6", "cell-7", "cell-8"];
 
+let playerTurn = false;
 cells.forEach(cell => {
   cell.addEventListener('click', (event) => {
     const clickedCell = event.target
-    const className = clickedCell.id;
-    console.log(className);
+    const cellText = clickedCell.textContent;
+
+    if(cellText == ""){
+      console.log("true")
+      if(playerTurn){
+
+        clickedCell.textContent = "x";
+        playerTurn = false;
+      }else{
+        clickedCell.textContent = "o";
+        playerTurn = true;
+      }
+
+    }
 
   });
 });
+
+//TODO: Add a winCheck for every move, and determine if there are no winners, make the AI move.
+// Fix the board text bug where you can change the player cells if yuo click it more than one time.
