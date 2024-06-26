d1c794d95001486ff9654f1b38d09b4777deb54b
loftyyyy <lofty08.business@gmail.com>
2024-06-26 19:05:17 +0800

    hello

--- js/app.js
+++ js/app.js
@@ -23,12 +23,15 @@ cells.forEach(cell => {
         clickedCell.textContent = "x";
         playerTurn = false;
       }else{
-        if(evaluate() !== 0){
-
+        if(evaluate(board) === 0){
+          clickedCell.textContent = "o";
+          playerTurn = true;
+        }else{
+          //create a win dialog.
+          setWin()
         }
-        clickedCell.textContent = "o";
-        playerTurn = true;
       }
+      setWin()
 
     }
 
@@ -53,13 +56,24 @@ function evaluate(board){
     // Vertical Win Check
     if((board[0][i].textContent === board[1][i].textContent) && (board[1][i].textContent === board[2][i].textContent) && board[0][i].textContent !== ""){
       console.log("Vertical Win")
-      return board[i][0].textContent === "x" ? -1 : 1;
+      return board[0][i].textContent === "x" ? -1 : 1;
     }
+  }
+
+  //Diagonals
+  if((board[0][0].textContent === board[1][1].textContent) && (board[1][1].textContent === board[2][2]) && board[1][1].textContent !== "" ){
+    return board[0][0].textContent === "x" ? -1 : 1;
+
+  }
+  if((board[0][2].textContent === board[1][1].textContent) && (board[1][1].textContent === board[2][0]) && board[1][1].textContent !== "" ){
+    return board[0][2].textContent === "x" ? -1 : 1;
+
   }
 
   for(let i = 0; i < board.length; i++){
     for(let j= 0; j < board[i].length; j++){
       if(board[i][j].textContent === ""){
+        console.log("available move")
         return 0;
       }
     }
@@ -69,5 +83,21 @@ function evaluate(board){
 
 }
 
+function setWin(){
+  const header = document.getElementById("winHeader");
+
+  if(evaluate(board) === -1){
+    header.textContent = "X won";
+
+  }
+  if(evaluate(board) === 1){
+    header.textContent = "O won";
+
+  }
+  if(evaluate(board) === 2){
+    header.textContent = "Draw";
+
+  }
+}
 //TODO: Add a winCheck for every move, and determine if there are no winners, make the AI move.
 // Fix the board text bug where you can change the player cells if yuo click it more than one time.
