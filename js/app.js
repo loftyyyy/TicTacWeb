f4cc12c1af0c3358ed2ae40e0b15ef56b426dff2
loftyyyy <lofty08.business@gmail.com>
2024-06-26 18:27:10 +0800

    hello

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
 const cells = document.querySelectorAll('td');
-const board = [[null,null,null], [null,null,null], [null,null,null]]
+let board = [[null,null,null], [null,null,null], [null,null,null]]
 
 let playerTurn = true;
 
 function clearBoard(){
-  for(i = 0; i < board.length; i++){
-    for(j = 0; j < board[i].length; j++){
+  for(let i = 0; i < board.length; i++){
+    for(let j = 0; j < board[i].length; j++){
 
+      board[i][j].textContent = "";
 
     }
 
   }
+  playerTurn = true;
 }
 cells.forEach(cell => {
   cell.addEventListener('click', (event) => {
     const clickedCell = event.target
     const cellText = clickedCell.textContent;
 
-    if(cellText == ""){
+    if(cellText === ""){
       if(playerTurn){
 
         clickedCell.textContent = "x";
@@ -41,12 +43,24 @@ cells.forEach(cell =>{
 })
 
 function evaluate(board){
-  //Vertical and Horizontal
-  for(i = 0; i < 3; i++){
-    if(board[i][0].textContent === board[i][1].textContent && board[i][0].textContent === board[i][2].textContent && board[i][0].textContent !== ""){
-      console.log("vertical win")
-      console.log(board[i][0].textContent)
+  for(let i = 0; i < 3; i++){
+    //Horizontal Win Check
+    if((board[i][0].textContent === board[i][1].textContent) && (board[i][1].textContent === board[i][2].textContent) && board[i][0].textContent !== ""){
+      console.log("Horizontal win")
     }
+
+    // Vertical Win Check
+    if((board[0][i].textContent === board[1][i].textContent) && (board[1][i].textContent === board[2][i].textContent) && board[0][i].textContent !== ""){
+      console.log("Horizontal win")
+    }
+  }
+
+  if((board[0][0].textContent === board[1][1].textContent) && (board[0][0].textContent === board[2][2]) && board[1][1].textContent !== ""){
+    console.log("Diagonal Win")
+  }
+
+  if((board[0][2].textContent === board[1][1].textContent) && (board[0][2].textContent === board[2][0]) && board[1][1].textContent !== ""){
+    console.log("Diagonal Win")
   }
 
 
