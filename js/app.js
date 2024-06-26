0286ad782809f5b03f0460f60b44f53a18bac39d
loftyyyy <lofty08.business@gmail.com>
2024-06-26 18:39:54 +0800

    hello

--- js/app.js
+++ js/app.js
@@ -6,11 +6,8 @@ let playerTurn = true;
 function clearBoard(){
   for(let i = 0; i < board.length; i++){
     for(let j = 0; j < board[i].length; j++){
-
       board[i][j].textContent = "";
-
     }
-
   }
   playerTurn = true;
 }
@@ -19,16 +16,19 @@ cells.forEach(cell => {
     const clickedCell = event.target
     const cellText = clickedCell.textContent;
 
+    //checks if the cell is empty, if yes proceed to make a move.
     if(cellText === ""){
       if(playerTurn){
 
         clickedCell.textContent = "x";
         playerTurn = false;
       }else{
+        if(evaluate() !== 0){
+
+        }
         clickedCell.textContent = "o";
         playerTurn = true;
       }
-      evaluate(board)
 
     }
 
@@ -47,22 +47,25 @@ function evaluate(board){
     //Horizontal Win Check
     if((board[i][0].textContent === board[i][1].textContent) && (board[i][1].textContent === board[i][2].textContent) && board[i][0].textContent !== ""){
       console.log("Horizontal win")
+      return board[i][0].textContent === "x" ? -1 : 1;
     }
 
     // Vertical Win Check
     if((board[0][i].textContent === board[1][i].textContent) && (board[1][i].textContent === board[2][i].textContent) && board[0][i].textContent !== ""){
-      console.log("Horizontal win")
+      console.log("Vertical Win")
+      return board[i][0].textContent === "x" ? -1 : 1;
     }
   }
 
-  if((board[0][0].textContent === board[1][1].textContent) && (board[0][0].textContent === board[2][2]) && board[1][1].textContent !== ""){
-    console.log("Diagonal Win")
+  for(let i = 0; i < board.length; i++){
+    for(let j= 0; j < board[i].length; j++){
+      if(board[i][j].textContent === ""){
+        return 0;
+      }
+    }
   }
 
-  if((board[0][2].textContent === board[1][1].textContent) && (board[0][2].textContent === board[2][0]) && board[1][1].textContent !== ""){
-    console.log("Diagonal Win")
-  }
-
+  return 2;
 
 }
 
